d4404b0b50053df476bec4c0b33120b63cdb7766
Omar AB <xroxrox@gmail.com>
2018-08-15 19:09:12 +0400

    Initial Commit

---
+++ RCLAPP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RCLAPP
+//
+//  Created by Omar Al Romaithi on 8/15/18.
+//  Copyright © 2018 UCarrot Software & Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
