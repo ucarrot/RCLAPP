714d87753072ea0440062a34f1c81a0f8817e4d9
Omar AB <xroxrox@gmail.com>
2018-08-16 22:37:39 +0400

    modification on the design (section 4. Create all list view controller)

---
+++ RCLAPP/AllListsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AllListsViewController.swift
+//  RCLAPP
+//
+//  Created by Omar Al Romaithi on 8/16/18.
+//  Copyright © 2018 UCarrot Software & Design. All rights reserved.
+//
+
+import UIKit
+
+class AllListsViewController: UIViewController {
+
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    //MARK: IBActions
+    
+    @IBAction func addBarItemButtonPressed(_ sender: Any) {
+        
+        
+    }
+    
+
+}
