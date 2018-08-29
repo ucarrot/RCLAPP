e2b9ce3824634331144638d144e24d3291073a37
Omar Al Romaithi <omar_r@mac.com>
2018-08-29 20:20:51 +0400

    adding search table view and new class (GroceryItemTableView) , (SearchItemViewController)

---
+++ RCLAPP/CustomCell/GroceryItemTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GroceryItemTableViewCell.swift
+//  RCLAPP
+//
+//  Created by Omar Al Romaithi on 8/29/18.
+//  Copyright © 2018 UCarrot Software & Design. All rights reserved.
+//
+
+import UIKit
+
+class GroceryItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ RCLAPP/SearchItemViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchItemViewController.swift
+//  RCLAPP
+//
+//  Created by Omar Al Romaithi on 8/29/18.
+//  Copyright © 2018 UCarrot Software & Design. All rights reserved.
+//
+
+import UIKit
+
+class SearchItemViewController: UIViewController {
+
+    @IBOutlet weak var addButtonOutlet: UIButton!
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    //MARK: UIActions
+    
+    @IBAction func addItemButtonPressed(_ sender: Any) {
+    }
+    
+
+}
